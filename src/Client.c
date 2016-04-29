3dfbd2f101728b962fad19e6be37e0e8bbed98e0
Oğuz Kerem Tural <oguz.kerem.tural@gmail.com>
2016-04-30 02:15:38 +0300

    Added authentication before main menu appears.

--- src/Client.c
+++ src/Client.c
@@ -9,6 +9,7 @@
 #include <errno.h>
 #include <time.h>
 #include <string.h>
+#include <time.h>
 
 #define BUFSIZE 1024
 #define STRING_SIZE 250
@@ -16,23 +17,28 @@
 
 /* Game Settings */
 typedef enum GameSigns {
-  EMPTY, O, X
+    EMPTY, O, X
 } GameSigns;
 
 typedef struct GameSettings {
-  int table[FIELD_SIZE][FIELD_SIZE];
-  char player_one_name[STRING_SIZE];
-  char player_two_name[STRING_SIZE];
-  char winner;
+    int table[FIELD_SIZE][FIELD_SIZE];
+    char player_one_name[STRING_SIZE];
+    char player_two_name[STRING_SIZE];
+    char winner;
 } GameSettings;
 
 /* Game room information */
 typedef struct SessionList {
-  int room_key;
-  char room_name[STRING_SIZE];
-  GameSettings game_settings;
-  SessionList *next;
-};
+    int room_key;
+    char room_name[STRING_SIZE];
+    GameSettings game_settings;
+    struct SessionList *next;
+} SessionList;
+
+typedef struct User {
+  char user_name[STRING_SIZE];
+  char password[STRING_SIZE];
+} User;
 
 /* It will initialize game settings before game starts */
 extern void init_game(GameSettings **game_settings);
@@ -49,72 +55,130 @@ extern void list_games(SessionList **game_list);
 /* Prints current game */
 extern void print_game();
 
-int main ( int argc, char **argv )
+int main(int argc, char **argv)
 {
-  if ( argc<=2 ) {
-    printf ( "Usage: ./program [IP Address] [PORT]\n" );
-    exit ( 1 );
-  }
-  char send_buf[BUFSIZE];
-  int socket_fd, fdmax, i;
-  
-  struct sockaddr_in server_addr;
-  
-  fd_set master;
-  fd_set read_fds;
-  
-  socket_fd=socket ( AF_INET,SOCK_STREAM,0 );
-  if ( socket_fd  ==-1 ) { //socket(domain,type,protocol) is defined by TCP (SOCK_STREAM) and returned to socket descriptor
-    printf ( "Socket cannot be created!!!\n" );
-    exit ( 1 );
-  }
-  
-  /* Network Family */
-  server_addr.sin_family=AF_INET;
-  
-  /* Port */
-  server_addr.sin_port=htons ( atoi ( argv[2] ) ); 
-  
-  /* IP Address */
-  server_addr.sin_addr.s_addr=inet_addr ( argv[1] ); 
-  
-  memset ( server_addr.sin_zero,'\0',sizeof server_addr.sin_zero ); //sin_zero is fulled by 0
-  
-  /* Connection Request */
-  int conn_result = connect ( socket_fd, ( struct sockaddr * ) &server_addr, sizeof ( struct sockaddr ) );
-  if ( conn_result ==-1 ) { 
-    printf ( "Error (2): Connection request has been failed!\n" );
-    exit ( 2 );
-  }
-  
-  /* File Descriptor Settings */
-  FD_ZERO ( &master );
-  FD_ZERO ( &read_fds );
-  FD_SET ( 0,&master ); //0 is added to master file
-  FD_SET ( sockfd,&master ); //sockfd is added to master file
-  
-  fdmax = socket_fd;
-  
-  while ( 1 ) {
-    read_fds = master;
-    if ( select ( fdmax + 1,&read_fds,NULL,NULL,NULL ) ==-1 ) { //track ready sockets to read,write, supports multiplexing, read_fds is updated
-      printf ( "Error (3): Sockets cannot be multiplexed!\n" );
-      exit ( 3 );
+    if (argc <= 2) {
+        printf("Usage: ./program [IP Address] [PORT]\n");
+        exit(1);
     }
-    for ( i=0; i<=fdmax; i++ )
-      if ( FD_ISSET ( i,&read_fds ) ) {
-        if ( i==0 ) { //ready for write
-          fgets ( send_buf,BUFSIZE,stdin );
+    char send_buf[BUFSIZE];
+    int socket_fd, fdmax;
+
+    struct sockaddr_in server_addr;
+
+    fd_set master;
+    fd_set read_fds;
+    
+    /* Socket creation */
+    socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_fd  == -1) {  
+        printf("Error (1): Socket creation failed!\n");
+        exit(1);
+    }
+
+    /* Network Family */
+    server_addr.sin_family = AF_INET;
+
+    /* Port */
+    server_addr.sin_port = htons(atoi(argv[2]));
+
+    /* IP Address */
+    server_addr.sin_addr.s_addr = inet_addr(argv[1]);
+
+    memset(server_addr.sin_zero, '\0', sizeof server_addr.sin_zero);  //sin_zero is fulled by 0
+
+    /* Connection Request */
+    int conn_result = connect(socket_fd, (struct sockaddr *) &server_addr, sizeof(struct sockaddr));
+    if (conn_result == -1) {
+        printf("Error (2): Connection request has been failed!\n");
+        exit(2);
+    }
+
+    /* File Descriptor Settings */
+    FD_ZERO(&master);
+    FD_ZERO(&read_fds);
+    FD_SET(0, &master);   
+    FD_SET(socket_fd, &master);  
+
+    fdmax = socket_fd;
+    
+    int i = 0, is_user_login = 0;
+    User user_info;
+    time_t auth;
+    struct tm auth_time;
+    char *position;
+    char recv_time[STRING_SIZE];
+    
+    /* TODO Cleanup the code a little bit after learn FD */
+    while (1) {
+        read_fds = master;
+
+        /* Track and try socket */
+        /*
+        if (select(fdmax + 1, &read_fds, NULL, NULL, NULL) == -1) {
+            printf("Error (3): Sockets cannot be multiplexed!\n");
+            exit(3);
+        }*/
+        
+        if (is_user_login != 1) {
+          printf("Username: ");
+          fgets(user_info.user_name, 250, stdin);
           
-          if ( strcmp ( send_buf,"quit\n" ) ==0 ) {
-            exit ( 0 );
-          } else if ( send ( socket_fd,send_buf,strlen ( send_buf ),0 ) ) //data is sent to server
-            printf ( ">message is sended\n" );
+          /* Remove trailing new line char */
+          if((position = strchr(user_info.user_name, '\n')) != NULL){
+            *position = '\0';
+          }
+          
+          printf("Password: ");
+          fgets(user_info.password, 250, stdin);
+          
+          /* Remove trailing new line char */
+          if((position = strchr(user_info.password, '\n')) != NULL){
+            *position = '\0';
+          }
+          
+          if (user_info.user_name[0] != '\0' && user_info.password[0] != '\0') {
+            send(socket_fd, &user_info, sizeof(User), 0);
+            
+            auth = time(NULL);
+            if (auth != -1){
+              auth_time = *localtime(&auth);
+              strftime(recv_time, STRING_SIZE, "%H:%M:%S", &auth_time);
+            }
+            
+            /* FIXME Problem with time formatting wonder why though no idea */
+            printf("%s authenticated at %s", user_info.user_name, recv_time);
+            is_user_login = 1;
+          }
         }
-      }
-      
-  }
-  printf ( "client-quited\n" );
-  close ( socket_fd ); //socket is closed
-  return 0;
+        
+        /* TODO Learn dafuq is this below */
+        /*
+        for (i = 0; i <= fdmax; i++)
+            if (FD_ISSET(i, &read_fds)) {
+              /* Ready for write   
+              if (i == 0) {
+                printf("[list, play]: ");
+                fgets(send_buf, BUFSIZE, stdin);
+                 
+                if (strcmp(send_buf, "quit\n") == 0) {
+                  exit(0);
+                /* Sending data to server 
+                } else if (send(socket_fd, send_buf, strlen(send_buf), 0)) {
+                   printf(">message is sended\n");
+                }
+            }
+        }*/
+    }
+    printf("Good bye! \n");
+    close(socket_fd);    //socket is closed
+    return 0;
 }
+
+void print_menu()
+{
+  printf("XOX Multi Showdown! \n");
+  printf("List Available Games \n");
+  printf("Quit \n");
+}
+
--- src/Server.c
+++ src/Server.c
@@ -7,131 +7,143 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <stdbool.h>
+#include <time.h>
 
 #define STRING_SIZE 250
 #define FIELD_SIZE 3
 
 /* Game Settings */
 typedef enum GameSigns {
-  EMPTY, O, X
+    EMPTY, O, X
 } GameSigns;
 
 typedef struct GameSettings {
-  int table[FIELD_SIZE][FIELD_SIZE];
-  char player_one_name[STRING_SIZE];
-  char player_two_name[STRING_SIZE];
-  char winner;
+    int table[FIELD_SIZE][FIELD_SIZE];
+    char player_one_name[STRING_SIZE];
+    char player_two_name[STRING_SIZE];
+    char winner;
 } GameSettings;
 
 /* Game room information */
 typedef struct SessionList {
-  int room_key;
-  char room_name[STRING_SIZE];
-  GameSettings game_settings;
-  SessionList *next;
-};
+    int room_key;
+    char room_name[STRING_SIZE];
+    GameSettings game_settings;
+    struct SessionList *next;
+} SessionList;
+
+/* User */
+typedef struct User {
+    char user_name[STRING_SIZE];
+    char user_pass[STRING_SIZE];
+} User;
 
 /* User list */
 typedef struct UserList {
-  char user_name[STRING_SIZE];
-  char user_pass[STRING_SIZE];
-  
-  UserList *next;
-};
+    User user;
+    struct UserList *next;
+} UserList;
 
-int main ( int argc, char **argv )
+int main(int argc, char **argv)
 {
-     if ( argc<=1 ) {
-          printf ( "Please give port number to listen\n" );
-          exit ( 1 );
-     }
+    if (argc <= 1) {
+        printf("Please give port number to listen\n");
+        exit(1);
+    }
 
-     fd_set master; //master file descriptor list
-     fd_set read_fds; //copy of master file descriptor list which is temp
+    fd_set master; //master file descriptor list
+    fd_set read_fds; //copy of master file descriptor list which is temp
 
-     struct sockaddr_in serveraddr;//server address
-     struct sockaddr_in clientaddr;//client address
+    struct sockaddr_in serveraddr;//server address
+    struct sockaddr_in clientaddr;//client address
 
-     int fdmax; //biggest file descriptor number
-     int listener;//socket descriptor which listens client
-     int newfd; //new socket which is accepted
+    int fdmax; //biggest file descriptor number
+    int listener;//socket descriptor which listens client
+    int newfd; //new socket which is accepted
 
-     char buf[1024];//buffer for data
-     int nbytes;//number of received bytes
-     int PORT=atoi ( argv[1] );
-     /* for setsockopt() SO_REUSEADDR, below */
-     int addrlen;
-     int i;
-     int yes=1;
+    char buf[1024];//buffer for data
+    int nbytes;//number of received bytes
+    int PORT = atoi(argv[1]);
+    /* for setsockopt() SO_REUSEADDR, below */
+    int addrlen;
+    int i;
+    int yes = 1;
 
-     FD_ZERO ( &master ); //master and temp is cleared
-     FD_ZERO ( &read_fds );
+    FD_ZERO(&master);    //master and temp is cleared
+    FD_ZERO(&read_fds);
 
-     if ( ( listener=socket ( AF_INET,SOCK_STREAM,0 ) ) ==-1 ) { //socket is defined TCP, and return socket descriptor for listening
-          // for UDP, use DATAGRAM instead of STREAM
-          printf ( "Socket cannot be created!!!\n" );
-          exit ( 1 ); //EXIT FAILURE
-     }
-     printf ( "Socket is created...\n" );
+    if ((listener = socket(AF_INET, SOCK_STREAM, 0)) == -1) {   //socket is defined TCP, and return socket descriptor for listening
+        // for UDP, use DATAGRAM instead of STREAM
+        printf("Socket cannot be created!!!\n");
+        exit(1);    //EXIT FAILURE
+    }
+    printf("Socket is created...\n");
 
-     /*if there exist such situation which is "address already in use" error message, even so use this socket */
-     if ( setsockopt ( listener,SOL_SOCKET,SO_REUSEADDR,&yes,sizeof ( int ) ) ==-1 ) { //socket,level,optname,optval,optlen are set
-          printf ( "Socket cannot be used!!!\n" );
-          exit ( 1 ); //EXIT FAILURE
-     }
-     printf ( "Socket is being used...\n" );
+    /*if there exist such situation which is "address already in use" error message, even so use this socket */
+    if (setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {    //socket,level,optname,optval,optlen are set
+        printf("Socket cannot be used!!!\n");
+        exit(1);    //EXIT FAILURE
+    }
+    printf("Socket is being used...\n");
 
-     serveraddr.sin_family=AF_INET;
-     serveraddr.sin_addr.s_addr=INADDR_ANY;//use my IP address
-     serveraddr.sin_port=htons ( PORT );
-     memset ( & ( serveraddr.sin_zero ),'\0',8 );
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_addr.s_addr = INADDR_ANY; //use my IP address
+    serveraddr.sin_port = htons(PORT);
+    memset(& (serveraddr.sin_zero), '\0', 8);
 
-     if ( bind ( listener, ( struct sockaddr * ) &serveraddr,sizeof ( serveraddr ) ) ==-1 ) { //socket and port is bind
-          printf ( "Binding cannot be done!!!\n" );
-          exit ( 1 ); //EXIT FAILURE
-     }
-     printf ( "Binding is done...\n" );
+    if (bind(listener, (struct sockaddr *) &serveraddr, sizeof(serveraddr)) == -1) {         //socket and port is bind
+        printf("Binding cannot be done!!!\n");
+        exit(1);    //EXIT FAILURE
+    }
+    printf("Binding is done...\n");
 
-     if ( listen ( listener,45 ) ==-1 ) { //listening, 45 connection is let in buffer
-          printf ( "" );
-          exit ( 1 ); //EXIT FAILURE
-     }
-     printf ( "Server is listening...\n" );
+    if (listen(listener, 45) == -1) {    //listening, 45 connection is let in buffer
+        printf("");
+        exit(1);    //EXIT FAILURE
+    }
+    printf("Server is listening...\n");
 
-     FD_SET ( listener,&master ); //add listener to master set
-     fdmax=listener; //biggest is listener now
+    FD_SET(listener, &master);   //add listener to master set
+    fdmax = listener; //biggest is listener now
+    User user_data;
+    struct tm auth_recv_time;
+    time_t recv_time_t;
+    char recv_time[STRING_SIZE];
+    for (;;) {
+        read_fds = master; //copy of master
+        if (select(fdmax + 1, &read_fds, NULL, NULL, NULL) == -1) { //track ready sockets to read,write, supports multiplexing, read_fds is updated
+            exit(1);    //EXIT FAILURE
+        }
+        for (i = 0; i <= fdmax; i++) { //all list is controlled
+            if (FD_ISSET(i, &read_fds)) {     //if it is in temp list
+                if (i == listener) { //if it is listener, there is new connection
+                    addrlen = sizeof(clientaddr);
 
-     for ( ;; ) {
-          read_fds=master;//copy of master
-          if ( select ( fdmax+1,&read_fds,NULL,NULL,NULL ) ==-1 ) { //track ready sockets to read,write, supports multiplexing, read_fds is updated
-               exit ( 1 ); //EXIT FAILURE
-          }
-          for ( i=0; i<=fdmax; i++ ) { //all list is controlled
-               if ( FD_ISSET ( i,&read_fds ) ) { //if it is in temp list
-                    if ( i==listener ) { //if it is listener, there is new connection
-                         addrlen=sizeof ( clientaddr );
+                    newfd = accept(listener, (struct sockaddr *) &clientaddr, &addrlen);   //accept connect request of client and return new socket for this connection
+                    FD_SET(newfd, &master);   // add new socket to master set
+                    if (newfd > fdmax)
+                        fdmax = newfd; //it is biggest socket number
 
-                         newfd=accept ( listener, ( struct sockaddr * ) &clientaddr,&addrlen ); //accept connect request of client and return new socket for this connection
-                         FD_SET ( newfd,&master ); // add new socket to master set
-                         if ( newfd>fdmax )
-                              fdmax=newfd;//it is biggest socket number
+                } else { //if it is not listener, there is data from client
+                    if ((nbytes = recv(i, &user_data, sizeof(User), 0)) <= 0) {     //from i. connection,socket
 
-                    } else { //if it is not listener, there is data from client
-                         if ( ( nbytes=recv ( i,buf,sizeof ( buf ),0 ) ) <=0 ) { //from i. connection,socket
+                        printf("Socket closed...\n");
+                        close(i);    //connection,socket closed
+                        FD_CLR(i, &master);   //it is removed from master set
 
-                              printf ( "Socket closed...\n" );
-                              close ( i ); //connection,socket closed
-                              FD_CLR ( i,&master ); //it is removed from master set
-
-                         } else { //if data is received from a client
-                              buf[nbytes]='\0';
-                              printf ( "%d says: %s \n",i,buf );
-                         }
+                    } else { //if data is received from a client
+                      recv_time_t = time(NULL);
+                      if (recv_time_t != -1) {
+                        auth_recv_time = *localtime(&recv_time_t);
+                        strftime(recv_time, STRING_SIZE, "%H:%M:%S", &auth_recv_time);
+                      }
+                      printf("%s joined at %s \n", user_data.user_name, recv_time);
                     }
-               }
-          }
-     }
-     return 0;
+                }
+            }
+        }
+    }
+    return 0;
 }
 
 
